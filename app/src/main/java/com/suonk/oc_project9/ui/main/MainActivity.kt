aa3e912c2b3759010282744f2002bca96526d9a3
Ken Suon <ken.su_dev@hotmail.com>
2023-02-01 16:10:19 +0100

    Ask permission to read pictures from gallery

--- app/src/main/java/com/suonk/oc_project9/ui/main/MainActivity.kt
+++ app/src/main/java/com/suonk/oc_project9/ui/main/MainActivity.kt
@@ -1,38 +1,51 @@
 package com.suonk.oc_project9.ui.main
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.os.Environment
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.suonk.oc_project9.R
 import com.suonk.oc_project9.databinding.ActivityMainBinding
-import com.suonk.oc_project9.ui.real_estates.details.RealEstateDetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private val viewModel by viewModels<RealEstateDetailsViewModel>()
+    private companion object {
+        private const val STORAGE_PERMISSION_CODE = 100
+        private const val TAG = "PERMISSION_TAG"
+    }
 
-    var uri: Uri? = null
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        requestPermission()
+    }
+
+    private fun displayUi() {
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
         setupActionBar(binding)
     }
 
-    //region =========================================== TOOLBAR ============================================
+    //region ================================================================ TOOLBAR ===============================================================
 
     private fun setupActionBar(binding: ActivityMainBinding) {
         binding.toolbar.apply {
@@ -41,6 +54,57 @@ class MainActivity : AppCompatActivity() {
         }
 
         supportActionBar?.title = ""
+    }
+
+    //endregion
+
+    //region ================================================================ REQUEST ===============================================================
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                val intent = Intent()
+                intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+                val uri = Uri.fromParts("package", this.packageName, null)
+                intent.data = uri
+                storageActivityResultLauncher.launch(intent)
+            } catch (e: Exception) {
+                Log.e(TAG, "requestPermission: ", e)
+                val intent = Intent()
+                intent.action = Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
+                storageActivityResultLauncher.launch(intent)
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
+                STORAGE_PERMISSION_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayUi()
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+    private val storageActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (Environment.isExternalStorageManager()) {
+                displayUi()
+            } else {
+                requestPermission()
+            }
+        } else {
+            requestPermission()
+        }
     }
 
     //endregion
